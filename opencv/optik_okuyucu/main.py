a52e13139c7738cf0a9507d3ce44a338a1e67c1a
Improvenss <gorkem_se_98@hotmail.com>
2022-03-24 01:35:14 +0300

    a_few_projects_added

---
+++ opencv/optik_okuyucu/main.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+import numpy as np
+
+datasets = 'data'
+
+
+
+cv.waitKey(0)
+cv.destroyAllWindows()
---
+++ xcode/1first/Shared/_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  _firstApp.swift
+//  Shared
+//
+//  Created by Görkem Sever on 23.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct _firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
